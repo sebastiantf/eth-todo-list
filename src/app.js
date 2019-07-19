fc7c0fcc55fb322cf5a8adba7f7bc9ac3da5085d
sebastiantf <sebastian.tharakan97@gmail.com>
2019-07-19 20:39:17 +0530

    loadWeb3 method

--- src/app.js
+++ src/app.js
@@ -1,12 +1,37 @@
 // App - app object
 App = {
     load: async () => {
-        console.log('app loading...');
+        await App.loadWeb3();
+    },
+
+    // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
+    loadWeb3: async () => {
+        // Modern dapp browsers...
+        if (window.ethereum) {
+            window.web3 = new Web3(ethereum);
+            try {
+                // Request account access if needed
+                await ethereum.enable();
+                // Acccounts now exposed
+                web3.eth.sendTransaction({/* ... */});
+            } catch (error) {
+                // User denied account access...
+            }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+            window.web3 = new Web3(web3.currentProvider);
+            // Acccounts always exposed
+            web3.eth.sendTransaction({/* ... */});
+        }
+        // Non-dapp browsers...
+        else {
+            console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+        }
     }
 }
 
 $(() => {   //DOMReady
-    console.log('dom ready...');
     $(window).load(() => {  //window.onload
         App.load();
     });
