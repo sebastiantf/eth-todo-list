5d52edcb6eac47aa0d4f8f1f37c04c6987c06dae
sebastiantf <sebastian.tharakan97@gmail.com>
2019-07-19 20:48:11 +0530

    loadAccount method

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 App = {
     load: async () => {
         await App.loadWeb3();
+        await App.loadAccount();
     },
 
     // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
@@ -28,6 +29,11 @@ App = {
         else {
             console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
         }
+    },
+
+    loadAccount : async () => {
+        App.account = web3.eth.accounts[0];
+        console.log(App.account);
     }
 }
 
